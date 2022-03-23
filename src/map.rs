a44959751f808c1b4d340a5d563f5d3a111b02c6
Barrett Helms <barrett.helms@salesloft.com>
2022-03-22 23:22:33 -0400

    Add map builder and camera view

--- src/map.rs
+++ src/map.rs
@@ -20,17 +20,33 @@ impl Map {
     }
 
     // iterating y first is faster with row-first striding due to memory cache usage
-    pub fn render(&self, ctx: &mut BTerm) {
-        for y in 0..SCREEN_HEIGHT {
-            for x in 0..SCREEN_WIDTH {
-                let idx = map_idx(x, y);
-                match self.tiles[idx] {
-                    TileType::Floor => ctx.set(x, y, YELLOW, BLACK, to_cp437('.')),
-                    TileType::Wall => ctx.set(x, y, GREEN, BLACK, to_cp437('#')),
+    pub fn render(&self, ctx: &mut BTerm, camera: &Camera) {
+        ctx.set_active_console(0);
+        for y in camera.top_y..camera.bottom_y {
+            for x in camera.left_x..camera.right_x {
+                if self.in_bounds(Point::new(x, y)) {
+                    let idx = map_idx(x, y);
+                    match self.tiles[idx] {
+                        TileType::Floor => ctx.set(
+                            x - camera.left_x,
+                            y - camera.top_y,
+                            WHITE,
+                            BLACK,
+                            to_cp437('.'),
+                        ),
+                        TileType::Wall => ctx.set(
+                            x - camera.left_x,
+                            y - camera.top_y,
+                            WHITE,
+                            BLACK,
+                            to_cp437('#'),
+                        ),
+                    }
                 }
             }
         }
     }
+
     pub fn in_bounds(&self, point: Point) -> bool {
         point.x >= 0 && point.x < SCREEN_WIDTH && point.y >= 0 && point.y < SCREEN_HEIGHT
     }
@@ -50,5 +66,5 @@ impl Map {
 
 // row-first striding
 pub fn map_idx(x: i32, y: i32) -> usize {
-    ((y * SCREEN_WIDTH) + x) as usize
+    (y * SCREEN_WIDTH + x) as usize
 }
